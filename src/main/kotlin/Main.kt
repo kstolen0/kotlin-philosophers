b0571eb0338d0c0e2ab75f36d4c84c0c53157008
Kristian Stolen <kstolen0@gmail.com>
2025-05-25 20:12:37 +0800

    use coroutine scope. deadlock works as expected now.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,44 @@
 package org.example
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import java.util.Date
 
 var dumplings = 100
 
+suspend fun main() {
+
+    val ch1 = Mutex(false)
+    val ch2 = Mutex(false)
+    val ch3 = Mutex(false)
+
+    val p1 = Philosopher("Aristotle", ch1, ch2)
+    val p2 = Philosopher("Rene", ch2, ch3)
+    val p3 = Philosopher("Jean", ch3, ch1)
+
+    withTiming {
+        coroutineScope {
+            launch { p1.eat() }
+            launch { p2.eat() }
+            launch { p3.eat() }
+        }
+    }
+}
+
+suspend fun withTiming(action: suspend () -> Unit) {
+    val start = Date().time
+    action()
+    println("completed in ${(Date().time - start) / 1000} seconds")
+}
+
 class Philosopher(val name: String, val leftChopstick: Mutex, val rightChopstick: Mutex) {
     suspend fun eat() {
         while (dumplings > 0) {
             leftChopstick.withLock {
                 println("$name picked up left chopstick")
-                delay(1)
                 rightChopstick.withLock {
                     println("$name picked up right chopstick")
                     dumplings--
@@ -28,33 +52,4 @@ class Philosopher(val name: String, val leftChopstick: Mutex, val rightChopstick
             delay(100)
         }
     }
-}
-
-fun main() {
-
-    val ch1 = Mutex(false)
-    val ch2 = Mutex(false)
-    val ch3 = Mutex(false)
-
-    val p1 = Philosopher("Aristotle", ch1, ch2)
-    val p2 = Philosopher("Rene", ch2, ch3)
-    val p3 = Philosopher("Jean", ch3, ch1)
-
-    runBlocking {
-        withTiming {
-            val eating1 = launch { p1.eat() }
-            val eating2 = launch { p2.eat() }
-            val eating3 = launch { p3.eat() }
-
-            eating1.join()
-            eating2.join()
-            eating3.join()
-        }
-    }
-}
-
-suspend fun withTiming(action: suspend () -> Unit) {
-    val start = Date().time
-    action()
-    println("completed in ${(Date().time - start) / 1000} seconds")
 }
