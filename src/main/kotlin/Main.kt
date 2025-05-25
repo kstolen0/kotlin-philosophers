20499b3cb9cbcef21b04303325f8188b5e1ef14a
Kristian Stolen <kstolen0@gmail.com>
2025-05-25 19:28:22 +0800

    add timing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
+import java.util.Date
+
 fun main() {
     println("hello world")
 }
+
+suspend fun withTiming(action: suspend () -> Unit) {
+    val start = Date().time
+    action()
+    println("Completed in ${(Date().time - start) / 1000} seconds")
+}
+
